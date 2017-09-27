9df9f2228b05a8bac2529a8e853cc0edb62c6388
Tu Nguyen <tounh17@tarleton.edu>
2017-09-26 22:55:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# CUDA Julia set Implementation on CUDA 9" 
---
+++ julia.cu
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../book/common/book.h"
+// #include "../book/common/cpu_bitmap.h"
+
+
+struct complex{
+  // internal variables
+  float r;
+  float i;
+  //Constructor
+  complex(float a, float b) : r(a), i(b) {}
+  //methods
+  void print(){printf("%f + %f*i", r, i);};
+  float magnitude(){
+    return (r*r) + (i*i);
+  }
+
+  //modify the actual + operator
+  complex operator+(const complex &b){
+    return complex(r*b.r, i*b.i);
+  }
+
+  //modify the actual * operator
+  complex operator*(const complex &b){
+    return complex(r*b.r - i*b.i, r*b.i - i*b.r);
+  }
+};
+
+int main(){
+  // printf("Hello");
+  complex a(1,2);
+  complex b(3,4);
+  (a*b).print();
+}
